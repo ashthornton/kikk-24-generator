fd273972ec7b98a4e0f7e4f6236fb3c28f2ca6e6
Ash Thornton <ashthornton0@gmail.com>
2024-05-15 15:14:30 +0100

    ash-03 updates

--- sketches/ash-03/sketch.fs
+++ sketches/ash-03/sketch.fs
@@ -1,49 +1,53 @@
 uniform float uTime;
 uniform vec2 uResolution;
 uniform vec3 uColor;
+uniform vec3 uBgColor;
+uniform float uRingAmount;
+uniform float uTimeOffset;
+uniform float uSpeed;
+uniform float uWaveNoiseStrength;
+uniform float uTextureNoiseStrength;
+uniform float uEdgeCurve;
+uniform float uSegments;
+uniform vec2 uOrigin;
 
 varying vec2 vUv;
 
 #define PI     3.14159265358
 #define TWO_PI 6.28318530718
 
-// #include "../../lygia/generative/snoise.glsl"
-#include "../../lygia/generative/worley.glsl"
+// #include "../../lygia/generative/pnoise.glsl"
+#include "../../lygia/generative/fbm.glsl"
+// #include "../../lygia/generative/worley.glsl"
 
 void main() {
-    vec2 uv = (2. * gl_FragCoord.xy - uResolution) / min(uResolution.x, uResolution.y);
+    vec2 uv = (2. * gl_FragCoord.xy - uResolution) / min(uResolution.x, uResolution.y) + uOrigin;
+    float time = uTime * 0.001 * uSpeed;
 
-    float time = uTime * 0.001;
+    float noise = fbm(vec3(vUv * 1., time * 0.1)) * 0.5 + 0.5;
+    float noise2 = fbm(vec3(uv * 20., time * 0.2)) * 0.5 + 0.5;
+    // noise = step(0.5, noise);
+
     float radsPercent = atan(uv.x, uv.y) / TWO_PI; // get angle to center
-    float dist = length(uv) * 2.; // multiply radius to achieve smaller rings
-    float ringSegments = 1. + floor(dist * 10.); // number of ring segments depends on radius
+    float dist = length(uv); // multiply radius to achieve smaller rings
+    float ringSegments = 0. + floor(dist * uRingAmount); // number of ring segments depends on radius
     time += sin(time * 0.010) * 10.; // remap time into an oscillation
-    time *= ringSegments * 0.002; // increase time/spin moving out from center
-    float ringRadsOffset = 0.; // possible unique offset per ring
-    radsPercent = mod(time + radsPercent + ringRadsOffset, 0.1); // rotate individual rings
-    // if (mod(ringSegments, 2.) == 1.)
-    //     ringSegments++; // make sure we have even number of segments
+    time *= ringSegments * 0.002 * uTimeOffset; // increase time/spin moving out from center
+    float ringRadsOffset = dist * uEdgeCurve + noise * uWaveNoiseStrength; // possible unique offset per ring
+    radsPercent = mod(time + radsPercent * uSegments + ringRadsOffset, 0.1); // rotate individual rings
     float segment = radsPercent * ringSegments; // progress around circle
-    float segmentNumber = floor(segment); // iterate over ring segments - get segment number
-    // segment -= segment * segmentNumber; // get segment progress
 
-    // float noise = snoise(vec3(uv * 1. + segment, time * 0.1)); // add noise to the rings
+    float finalNoise = (1. - clamp(segment, 0., 1.)) + noise2 * uTextureNoiseStrength;
 
-    // gl_FragColor = vec4(1. - segment, 0., 0., 1.);
-    // gl_FragColor = vec4(noise, 0., 0., 1.);
+    vec3 color = uBgColor;
 
-    // float time = uTime * 0.0001;
-
-    float noise = worley(vec3(segment + vUv * 3., time * 25.));
-    float noise2 = worley(vec3(segment + vUv * 5.125, time * 25.));
-
-    float finalNoise = noise * noise2 * (1. - clamp(segment, 0., 1.));
-
-    vec3 color = vec3(0.91, 0.74, 0.82);
+    // color += noise;
 
     color = mix(color, uColor, finalNoise);
 
     gl_FragColor = vec4(color, 1.0);
 
-    // #include <colorspace_fragment>
+    // gl_FragColor = vec4(vec3(segment), 1.);
+
+    #include <colorspace_fragment>
 }
--- sketches/ash-03/sketch.js
+++ sketches/ash-03/sketch.js
@@ -3,9 +3,103 @@ import fragmentShader from './sketch.fs'
 
 export let props = {
 	color: {
-		value: '#39a9fe',
+		value: '#f2c2d2',
 		onChange: ({ value }) => {
 			uniforms.uColor.value.set(value)
+		}
+	},
+	bgColor: {
+		value: '#2373df',
+		onChange: ({ value }) => {
+			uniforms.uBgColor.value.set(value)
+		}
+	},
+	origin: {
+		value: [0, 0],
+		params: {
+			min: -5,
+			max: 5,
+			step: 0.01
+		},
+		onChange: ({ value }) => {
+			uniforms.uOrigin.value.set(...value)
+		}
+	},
+	ringAmount: {
+		value: 10,
+		params: {
+			min: 0,
+			max: 250,
+			step: 1
+		},
+		onChange: ({ value }) => {
+			uniforms.uRingAmount.value = value
+		}
+	},
+	timeOffset: {
+		value: 1,
+		params: {
+			min: 0,
+			max: 10,
+			step: 0.001
+		},
+		onChange: ({ value }) => {
+			uniforms.uTimeOffset.value = value
+		}
+	},
+	speed: {
+		value: 1,
+		params: {
+			min: 0,
+			max: 10,
+			step: 0.001
+		},
+		onChange: ({ value }) => {
+			uniforms.uSpeed.value = value
+		}
+	},
+	waveNoiseStrength: {
+		value: 0.,
+		params: {
+			min: 0,
+			max: 2,
+			step: 0.001
+		},
+		onChange: ({ value }) => {
+			uniforms.uWaveNoiseStrength.value = value
+		}
+	},
+	textureNoiseStrength: {
+		value: 0.07,
+		params: {
+			min: 0,
+			max: 2,
+			step: 0.001
+		},
+		onChange: ({ value }) => {
+			uniforms.uTextureNoiseStrength.value = value
+		}
+	},
+	edgeCurve: {
+		value: 0.328,
+		params: {
+			min: -2,
+			max: 2,
+			step: 0.001
+		},
+		onChange: ({ value }) => {
+			uniforms.uEdgeCurve.value = value
+		}
+	},
+	segments: {
+		value: 3,
+		params: {
+			min: 0,
+			max: 20,
+			step: 0.1
+		},
+		onChange: ({ value }) => {
+			uniforms.uSegments.value = value
 		}
 	}
 }
@@ -14,7 +108,16 @@ let camera
 let uniforms = {
 	uResolution: { value: new THREE.Vector2() },
 	uTime: { value: 0 },
-	uColor: { value: new THREE.Color(props.color.value) }
+	uColor: { value: new THREE.Color(props.color.value) },
+	uBgColor: { value: new THREE.Color(props.bgColor.value) },
+	uOrigin: { value: new THREE.Vector2(...props.origin.value) },
+	uRingAmount: { value: props.ringAmount.value },
+	uTimeOffset: { value: props.timeOffset.value },
+	uSpeed: { value: props.speed.value },
+	uWaveNoiseStrength: { value: props.waveNoiseStrength.value },
+	uTextureNoiseStrength: { value: props.textureNoiseStrength.value },
+	uEdgeCurve: { value: props.edgeCurve.value },
+	uSegments: { value: props.segments.value }
 }
 
 /**
